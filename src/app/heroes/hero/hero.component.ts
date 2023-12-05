3b7b3a192a4554e7220a9dda5b07bf533ade2a47
Gabriel Zapata <sr.gabrielzm@gmail.com>
2023-12-04 22:41:18 -0500

    Fin de las bases de Angular

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+
+  public name: string = 'ironman';
+  public age: number = 42;
+
+
+  get capitalizeName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return ` ${this.name} - ${this.age} `
+  }
+
+
+  changeHero(): void {
+    this.name = 'Batman';
+  }
+  changeAge(): void {
+    this.age = 33;
+  }
+
+  reset() {
+    this.name = 'ironman';
+    this.age = 42;
+  }
+}
